ce468bfd428be344219831a8dc4d9cd90a1d8665
ndamd <ndamd@DESKTOP-2US5KLR>
2023-03-18 08:56:04 -0700

    update

---
+++ QAAutomation/src/java_package/Helloclass.java
@@ -0,0 +1,10 @@
+package java_package;
+
+public class Helloclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello i am Derick");
+	}
+
+}
